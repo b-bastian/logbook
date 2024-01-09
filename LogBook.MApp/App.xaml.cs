ae5971df4cdda25be8307d984638297edec3a741
bastian-js <admin@bbastian.xyz>
2024-01-09 10:51:08 +0100

    first commit

---
+++ LogBook.MApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace LogBook.MApp;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ LogBook.MApp/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace LogBook.MApp;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
