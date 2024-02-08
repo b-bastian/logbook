e31febec6573f74b016250d7cc0995ba22dd88bb
bastian-js <admin@bbastian.xyz>
2024-02-08 08:28:00 +0100

    add mainviewmodel to maui

--- LogBook.MApp/MauiProgram.cs
+++ LogBook.MApp/MauiProgram.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using LogBook.MApp.ViewModles;
+using Microsoft.Extensions.Logging;
 
 namespace LogBook.MApp;
 
@@ -14,6 +15,9 @@ public static class MauiProgram
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
 			});
+
+		builder.Services.AddSingleton<MainViewModel>();
+		builder.Services.AddSingleton<MainPage>();
 
 #if DEBUG
 		builder.Logging.AddDebug();
--- LogBook.MApp/ViewModels/MainViewModel.cs
+++ LogBook.MApp/ViewModels/MainViewModel.cs
@@ -1,11 +1,10 @@
 using CommunityToolkit.Mvvm.Input;
 using CommunityToolkit.Mvvm.ComponentModel;
 
-namespace LogBook.MApp.ViewModles
-{
-    public partial class MainViewModel : ObservableObject
-    {
+namespace LogBook.MApp.ViewModels;
 
-    }
+public partial class MainViewModel : ObservableObject
+{
+    public string Header => "Fahrtenbuch";
 }
 
