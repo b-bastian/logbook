e8b2a24ef4c604438cd3f9984d963e059ec35b33
bastian-js <admin@bbastian.xyz>
2024-02-08 08:00:00 +0100

    add description to entry

--- LogBook.Cli/Program.cs
+++ LogBook.Cli/Program.cs
@@ -7,14 +7,17 @@ Console.WriteLine("Willkommen im Fahrtenbuch!");
 string path = "logbook.xml";
 
 IRepository repository = new XmlRepository(path);
-List<Entry> entries = repository.GetAll();
 
 repository.Add(new Entry(DateTime.Now, DateTime.Now.AddHours(2), 25000, 25180, "ZE-XY123", "Zell am See", "München"));
 
-Entry entrySaalfelden = new Entry(DateTime.Now.AddDays(3), DateTime.Now.AddDays(3).AddMinutes(20), 25500, 25514, "ZE-XY123", "Zell am See", "Saalfelden");
-entrySaalfelden.Description = "hi";
+Entry entrySaalfelden = new Entry(DateTime.Now.AddDays(3), DateTime.Now.AddDays(3).AddMinutes(20), 25500, 25514, "ZE-XY123", "Zell am See", "Saalfelden")
+{
+	Description = "Fahrt nach Saalfelden"
+};
 
 repository.Add(entrySaalfelden);
+
+List<Entry> entries = repository.GetAll();
 
 foreach (var item in entries)
 {
